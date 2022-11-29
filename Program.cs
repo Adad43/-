af410192937390bd4b01b052a676703707b73c79
Adad43 <118421216+Adad43@users.noreply.github.com>
2022-11-29 15:07:30 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using InspectorLib;
+using System;
+
+namespace KlimovInspectorLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp tom = new("");//Обращение к библиотеке
+            tom.Conclusionofthemessage();//Вывод библиотеки
+
+        }
+    }
+}
